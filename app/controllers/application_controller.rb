adc04871339c54fa2a192e9a33efd87cc7811953
ReadingPig <zyzpop830203@163.com>
2017-06-20 21:43:43 +0800

    implement part2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #################################
+  def require_is_admin
+    if !current_user.admin?####current_user.email != 'xdite@growth.school'
+      flash[:alert] = 'You are not admin'
+      redirect_to root_path
+    end
+  end
+  #################################
 end
