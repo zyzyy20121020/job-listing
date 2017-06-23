4c0744371789026af23d11bf7f0d228ad18962ff
ReadingPig <zyzpop830203@163.com>
2017-06-23 13:12:20 +0800

    implement part5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :update]
   before_action :require_is_admin
+  layout "admin"
 
   def show
     @job = Job.find(params[:id])
@@ -44,7 +45,19 @@ class Admin::JobsController < ApplicationController
   end
 
   #################################
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
 
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+
+    redirect_to :back
+  end
 
   private
   def job_params
