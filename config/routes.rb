06ddbadb92651ee0c5856079c1755424f8528202
ReadingPig <zyzpop830203@163.com>
2017-06-30 21:44:18 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'jobs#index'
-  
+
   resources :jobs do
     resources :resumes
   end
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
 
